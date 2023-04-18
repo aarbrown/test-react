e171721d808db460825d6e08fefe8a44bbaa7931
Aaron Brown <abrown7@instructors.2u.com>
2023-04-17 22:04:56 -0400

    changes

---
+++ src/components/IssueItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// Issue item accepts a single issue as a prop
+// This component is solely responsible for displaying the specific information for a given issue
+const IssueItem = ({ issue }) => {
+  return (
+    <div className="item">
+      <i className="large github middle aligned icon"></i>
+      <span>fails snapshot test now</span>
+      <div className="content">
+        <a
+          className="header"
+          href={issue.html_url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {issue.title}
+        </a>
+        <div className="description">{issue.description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default IssueItem;
